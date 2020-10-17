413e868ef45883f41f97efd9b780dc7cd26c0217
MSI\moneo <jvr.moneo@gmail.com>
2020-10-17 13:16:13 +0200

    estilos css

--- ts/index.ts
+++ ts/index.ts
@@ -77,9 +77,11 @@ class ProductSolid extends Product {
 
 class ProductLiquid extends Product {
     private _ml: number;
-    constructor(id: number, name: string, units: number, prize: number, src: string, ml: number) {
+    private _isAlcoholic: boolean;
+    constructor(id: number, name: string, units: number, prize: number, src: string, ml: number, isAlcoholic: boolean) {
         super(id, name, units, prize, src);
         this._ml = ml;
+        this._isAlcoholic = isAlcoholic;
     }
 
     get ml(): number {
@@ -87,6 +89,14 @@ class ProductLiquid extends Product {
     }
     set ml(ml: number) {
         this._ml = ml;
+    }
+
+    get isAlcoholic(): boolean {
+        return this._isAlcoholic;
+    }
+
+    set isAlcoholic(isAlcoholic: boolean) {
+        this._isAlcoholic = isAlcoholic;
     }
 }
 
@@ -142,22 +152,22 @@ const product5 = new ProductSolid(5, 'Mikado', 20, 1.00, 'image/mikado.png', 75)
 const product6 = new ProductSolid(6, 'Pringles', 20, 1.25, 'image/pringles.png', 40);
 const product7 = new ProductSolid(7, 'Donuts', 20, 1.50, 'image/donuts.png', 150);
 const product8 = new ProductSolid(8, 'Palomitas', 20, 1.50, 'image/palomitas.png', 150);
-const product9 = new ProductSolid(9, 'Ruffles Original', 20, 1.50, 'image/ruffles.png', 150);
-const product10 = new ProductSolid(10, 'Cheetos', 20, 1.50, 'image/cheetos.png', 150);
-const product11 = new ProductSolid(11, 'Doritos', 20, 1.50, 'image/doritos.png', 150);
-const product12 = new ProductSolid(12, 'KitKat', 20, 1.50, 'image/kitkat.png', 150);
+// const product9 = new ProductSolid(9, 'Ruffles Original', 20, 1.50, 'image/ruffles.png', 150);
+// const product10 = new ProductSolid(10, 'Cheetos', 20, 1.50, 'image/cheetos.png', 150);
+// const product11 = new ProductSolid(11, 'Doritos', 20, 1.50, 'image/doritos.png', 150);
+// const product12 = new ProductSolid(12, 'KitKat', 20, 1.50, 'image/kitkat.png', 150);
 
-const product13 = new ProductLiquid(13, 'Coca-cola', 20, 1.00, 'image/cocacola.png', 33);
-const product14 = new ProductLiquid(14, 'Fanta limón', 20, 1.00, 'image/fantalimon.png', 33);
-const product15 = new ProductLiquid(15, 'Pepsi', 20, 1.00, 'image/pepsi.png', 33);
-const product16 = new ProductLiquid(16, 'Agua', 20, 0.75, 'image/agua.png', 33);
+const product9 = new ProductLiquid(9, 'Coca-cola', 20, 1.00, 'image/cocacola.png', 33, false);
+const product10 = new ProductLiquid(10, 'Heineken', 20, 1.00, 'image/heineken.png', 33, true);
+const product11 = new ProductLiquid(11, 'Pepsi', 20, 1.00, 'image/pepsi.png', 33, false);
+const product12 = new ProductLiquid(12, 'Agua', 20, 0.75, 'image/agua.png', 33, false);
 
 //Array de productos
 const products = [
-    [product1, product2, product3, product13],
-    [product4, product5, product6, product14],
-    [product7, product8, product9, product15],
-    [product10, product11, product12, product16]];
+    [product1, product2, product9],
+    [product3, product4, product10],
+    [product5, product6, product11],
+    [product7, product8, product12]];
 
 $('#loading').show();
 $('#loading').hide();
